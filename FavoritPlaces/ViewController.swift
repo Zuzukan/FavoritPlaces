b26d8f0c8ce1afab1b85c5150a643f7a4154bf4d
BihunRuslan <bigun.ruslan@gmail.com>
2022-01-19 11:48:12 +0400

    Initial Commit

---
+++ FavoritPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavoritPlaces
+//
+//  Created by Ruslan Bihun on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
